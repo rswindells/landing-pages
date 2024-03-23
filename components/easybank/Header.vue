2fd8e13024c591948a7416e539bf96c991af8e95
Ross Swindells <99806297+rswindells@users.noreply.github.com>
2024-03-23 16:07:17 +0000

    feat: add header component

---
+++ components/easybank/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header class="bg-white drop-shadow-lg">
+        <div class="container mx-auto flex items-center justify-between py-4">
+            <div>
+                <img src="~/assets/images/logo.svg" alt="Easybank logo" />
+            </div>
+            <nav class="text-[#9597A5] text-sm hidden sm:block">
+                <ul class="flex items
+                    -center gap-x-[1.875rem]">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                    <li>
+                        <a href="#">Blog</a>
+                    </li>
+                    <li>
+                        <a href="#">Careers</a>
+                    </li>
+                </ul>
+            </nav>
+            <button
+                class="bg-brand-gradient text-white text-sm px-[2.125rem] py-2 rounded-full leading-[28px] font-bold">Request
+                Invite</button>
+        </div>
+    </header>
+</template>
