c50e1d5fc94596e940c5c028b91cffb1815533b5
Ross Swindells <99806297+rswindells@users.noreply.github.com>
2024-03-23 16:05:52 +0000

    feat: add tailwind css file, custom config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,32 @@ export default defineNuxtConfig({
   experimental: {
     payloadExtraction: true,
   },
+  tailwindcss: {
+    exposeConfig: true,
+    config: {
+      theme: {
+        extend: {
+          colors: {
+            "brand-primary-blue": "hsl(233, 26%, 24%)",
+            "brand-primary-green": "hsl(136, 65%, 51%)",
+            "brand-primary-cyan": "hsl(192, 70%, 51%)",
+            "brand-neutral-blue": "hsl(233, 8%, 62%)",
+            "brand-neutral-light-blue": "hsl(220, 16%, 96%)",
+            "brand-neutral-light-gray": "hsl(0, 0%, 98%)",
+            "brand-neutral-white": "hsl(0, 0%, 100%)",
+          },
+          fontSize: {
+            "body-copy": "1.125rem",
+          },
+          screens: {
+            mobile: "375px",
+          },
+          backgroundImage: {
+            "brand-gradient":
+              "linear-gradient(160deg, #33D35E 0%, #2AB6D9 100%)",
+          },
+        },
+      },
+    },
+  },
 });
